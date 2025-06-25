4ade9329127ccc4134d3886b3b44c277da2cc5bb
AnhVaccari <anh.vaccari@orange.fr>
2025-06-25 14:38:50 +0200

    Finalize homepage layout with pie chart

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
@@ -6,6 +6,7 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
@@ -15,4 +16,28 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
   }
+
+  view: [number, number] = [700, 400];
+
+  pieData = [
+    {
+      name: 'USA',
+      value: 120,
+    },
+    {
+      name: 'China',
+      value: 100,
+    },
+    {
+      name: 'Germany',
+      value: 80,
+    },
+    {
+      name: 'France',
+      value: 60,
+    },
+  ];
+
+  showLabels = true;
+  isDoughnut = false;
 }
