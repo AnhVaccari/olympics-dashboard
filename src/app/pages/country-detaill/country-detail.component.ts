1f369053ca38870c9b43325a4835d3101050b829
AnhVaccari <anh.vaccari@orange.fr>
2025-06-26 12:05:00 +0200

    style : update detail page design and add line chart

--- src/app/pages/country-detaill/country-detail.component.ts
+++ src/app/pages/country-detaill/country-detail.component.ts
@@ -11,6 +11,17 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 export class CountryDetailComponent implements OnInit {
   countryId!: number;
   countryData?: OlympicCountry;
+  view: [number, number] = [700, 300];
+  // options
+  legend = false;
+  showLabels = true;
+  xAxis = true;
+  yAxis = true;
+  showYAxisLabel = true;
+  showXAxisLabel = true;
+  xAxisLabel: string = 'Dates';
+  //yAxisLabel: string = 'Population';
+  //timeline = true;
 
   constructor(
     private route: ActivatedRoute,
@@ -28,4 +39,132 @@ export class CountryDetailComponent implements OnInit {
       }
     });
   }
+
+  lineData = [
+    {
+      name: 'Montenegro',
+      series: [
+        {
+          value: 6668,
+          name: '2016-09-16T18:25:16.128Z',
+        },
+        {
+          value: 5969,
+          name: '2016-09-19T12:06:10.528Z',
+        },
+        {
+          value: 5304,
+          name: '2016-09-17T14:44:42.599Z',
+        },
+        {
+          value: 3100,
+          name: '2016-09-21T17:14:24.384Z',
+        },
+        {
+          value: 6585,
+          name: '2016-09-22T16:57:30.687Z',
+        },
+      ],
+    },
+    {
+      name: 'Libya',
+      series: [
+        {
+          value: 4111,
+          name: '2016-09-16T18:25:16.128Z',
+        },
+        {
+          value: 2174,
+          name: '2016-09-19T12:06:10.528Z',
+        },
+        {
+          value: 2180,
+          name: '2016-09-17T14:44:42.599Z',
+        },
+        {
+          value: 2531,
+          name: '2016-09-21T17:14:24.384Z',
+        },
+        {
+          value: 6688,
+          name: '2016-09-22T16:57:30.687Z',
+        },
+      ],
+    },
+    {
+      name: 'San Marino',
+      series: [
+        {
+          value: 5448,
+          name: '2016-09-16T18:25:16.128Z',
+        },
+        {
+          value: 6095,
+          name: '2016-09-19T12:06:10.528Z',
+        },
+        {
+          value: 4945,
+          name: '2016-09-17T14:44:42.599Z',
+        },
+        {
+          value: 2871,
+          name: '2016-09-21T17:14:24.384Z',
+        },
+        {
+          value: 5040,
+          name: '2016-09-22T16:57:30.687Z',
+        },
+      ],
+    },
+    {
+      name: 'Samoa',
+      series: [
+        {
+          value: 2013,
+          name: '2016-09-16T18:25:16.128Z',
+        },
+        {
+          value: 6906,
+          name: '2016-09-19T12:06:10.528Z',
+        },
+        {
+          value: 6696,
+          name: '2016-09-17T14:44:42.599Z',
+        },
+        {
+          value: 2847,
+          name: '2016-09-21T17:14:24.384Z',
+        },
+        {
+          value: 2931,
+          name: '2016-09-22T16:57:30.687Z',
+        },
+      ],
+    },
+    {
+      name: 'Sri Lanka',
+      series: [
+        {
+          value: 3971,
+          name: '2016-09-16T18:25:16.128Z',
+        },
+        {
+          value: 6181,
+          name: '2016-09-19T12:06:10.528Z',
+        },
+        {
+          value: 2070,
+          name: '2016-09-17T14:44:42.599Z',
+        },
+        {
+          value: 3956,
+          name: '2016-09-21T17:14:24.384Z',
+        },
+        {
+          value: 4958,
+          name: '2016-09-22T16:57:30.687Z',
+        },
+      ],
+    },
+  ];
 }
